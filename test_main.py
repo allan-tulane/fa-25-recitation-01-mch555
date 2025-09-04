9fd4885ba42341c143e4e1b2836a5924004b57d8
mch555 <148922167+mch555@users.noreply.github.com>
2025-09-04 12:54:56 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -14,6 +14,8 @@ def test_binary_search():
 	assert binary_search([1,2,3,4,5], 1) == 0
 	assert binary_search([1,2,3,4,5], 6) == -1
 	### TODO: add two more tests here.
+	assert binary_search([-3, -2, -1, 0, 1, 2], -2) == 1
+	assert binary_search([-3, -2, -1, 0, 1, 2], 3) == -1
 
 	###
 
